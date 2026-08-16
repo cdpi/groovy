45c59cc9a82aa58d494e320175ef228297f07686
fabricetheytaz <fabrice.theytaz@gmail.com>
2026-08-16 17:32:50 +0200

    Regroupement (en cours) dans un seul SVG (SVGFont avec Inkscape)

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-svgs2ttf groovy.json
+svg2ttf groovy.svg fonts/groovy.ttf
 
-cp fonts/groovy.ttf docs/fonts/groovy.ttf
-cp fonts/groovy.otf docs/fonts/groovy.otf
-cp fonts/groovy.woff docs/fonts/groovy.woff
-cp fonts/groovy.woff2 docs/fonts/groovy.woff2
+#cp fonts/groovy.ttf docs/fonts/groovy.ttf
---
+++ old/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+svgs2ttf groovy.json
+
+cp fonts/groovy.ttf docs/fonts/groovy.ttf
+cp fonts/groovy.otf docs/fonts/groovy.otf
+cp fonts/groovy.woff docs/fonts/groovy.woff
+cp fonts/groovy.woff2 docs/fonts/groovy.woff2
